c64016af95d5012fcc19573af5bf27bf47b0afbb
David Dickinson <rnddave@gmail.com>
2023-12-03 12:10:26 +0000

    day 3 scratchpad created with my initial thoughts of I HAVE NO IDEA WHAT TO DO HERE recorded for no good reason

---
+++ 2023-12-03-scratchpad.py
@@ -0,0 +1,8 @@
+# what the actual fudge - I have no idea how to even start this one!!! 
+
+# read the input
+# scan multi-line - multi-direction at same time ( ???????? )
+# record the scores 
+# (this bit seems inceonsequential at the moment, I can't even begin to think about how
+# I would read a fdile in different directions)
+
