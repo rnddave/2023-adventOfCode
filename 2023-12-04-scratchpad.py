ccd96fe15103285710d1b40eef69a958096ad2c2
David Dickinson <rnddave@gmail.com>
2023-12-04 09:19:17 +0000

    day 4 solution file

---
+++ 2023-12-04-scratchpad.py
@@ -0,0 +1,6 @@
+# read a line
+# split at the | (pipe)
+# compare LEFT | RIGHT 
+# count number of matches (per line) doubling as we go
+# count total number of matches (sum of all the winning lines)
+
