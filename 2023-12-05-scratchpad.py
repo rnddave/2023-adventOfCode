596507b4cf7f9d87e1c234191d087cfa41e1a0ee
David Dickinson <rnddave@gmail.com>
2023-12-05 21:01:51 +0000

    day 5 part 2 complete - wuth about 90% of part 2 coming from reddit to be honest

--- 2023-12-05-scratchpad.py
+++ 2023-12-05-scratchpad.py
@@ -32,5 +32,74 @@ def day5PartOne(seeds, maps):
 
     return stupidVariableName
 
-if __name__ == "__main__":
-    print(day5PartOne(seeds, maps)) # 173706076
+print('----------------------------------')
+print('------------ PART ONE ------------')
+print('----------------------------------')
+
+print(day5PartOne(seeds, maps)) # 173706076 = correct
+
+# print('----------------------------------')
+# print('------------ PART TWO ------------')
+# print('----------------------------------')
+
+
+def onThe5thDayOfXmas(a,b,c,d):
+    return not (b < c or d < a)
+
+def day5PartTw0(seeds, maps):
+    
+    # thank you reddit tips
+    redditRange = []
+
+    for element in range(0, len(seeds), 2):
+        redditRange.append((seeds[element], seeds[element] + seeds[element + 1] - 1))
+
+    LINES.pop(0)
+
+    for workingGroup in LINES:
+        workingGroup = workingGroup.split("\n")
+        values = workingGroup[1:]
+        doppleGanger = seeds.copy()
+
+        newRedditRange = []
+
+        for fiveGoldenRings in values:
+            d,s,r = fiveGoldenRings.split()
+            d,s,r = int(d), int(s), int(r)
+
+            for element in range(len(seeds)):
+
+                if s <= seeds[element] < s+r:
+                    doppleGanger[element] += d - s
+
+            e = s + r - 1
+
+            theNewNewRedditList = []
+
+            for spread in redditRange:
+
+                if onThe5thDayOfXmas(s, e, spread[0], spread[1]):
+                    c1 = max(s,spread[0])
+                    c2 = min(e,spread[1])
+                    newRedditRange.append((c1 + d - s, c2 + d - s))
+
+                    if spread[0] < c1:
+                        theNewNewRedditList.append((spread[0],c1-1))
+
+                    if spread[1] > c2:
+                        theNewNewRedditList.append((c2+1,spread[1]))
+
+                else:
+                    theNewNewRedditList.append(spread)
+
+            redditRange = theNewNewRedditList
+
+        redditRange = newRedditRange + theNewNewRedditList
+
+    print(min(redditRange)[0])
+
+print('----------------------------------')
+print('------------ PART TWO ------------')
+print('----------------------------------')
+
+day5PartTw0(seeds, maps)    # 11611182 = CORRECT - but the answer was derived by sneaky peaking the reddit tips
